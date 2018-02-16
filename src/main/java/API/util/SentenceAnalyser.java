a33fe1e903d3fc652cdf7bb919191f4c771bfe52
Sam <sam.grennan8@gmail.com>
2018-02-16 21:20:46 +1100

    implemented lookahead for determiner input

--- src/main/java/API/util/SentenceAnalyser.java
+++ src/main/java/API/util/SentenceAnalyser.java
@@ -1,8 +1,14 @@
 package API.util;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SentenceAnalyser {
+
+    private String[] nouns = new String[20];
+    private String[] determiners = new String[20];
+    private String[] adjectives = new String[20];
 
 
     /**
@@ -12,6 +18,7 @@ public class SentenceAnalyser {
      * @return
      */
     public boolean validate(String[] sentence) {
+
         return true;
     }
 
@@ -26,7 +33,38 @@ public class SentenceAnalyser {
      * @return
      */
     public Map<String, String[]> lookahead(String word) {
+        Map<String, String[]> detLookahead = new HashMap<>();
 
+        if (Arrays.asList(determiners).contains(word)) {
+            detLookahead.put(word, adjsnouns(nouns, adjectives));
+            return detLookahead;
+        }
+
+    }
+
+
+    /**
+     * Helper method that concatenates array of nouns and adjectives
+     *
+     * Nouns and adjectives are the only word type that can follow a determiner such as "the" or "a"
+     *
+     * @param adj array of adjs
+     * @param nouns array of nouns
+     * @return arrays concatenated as one array
+     */
+    public String[] adjsnouns(String[] adj, String[] nouns) {
+        String[] result = new String[nouns.length + adj.length];
+        for (int i = 0; i < nouns.length; i++) {
+            result[i] = nouns[i];
+        }
+        for (int j = 0; j < adj.length; j++) {
+            result[j + 20] = adj[j];
+        }
+        return result;
+    }
+
+
+    public Map<String, String[]> detLookahead(String word) {
         return null;
     }
 
@@ -43,6 +81,11 @@ public class SentenceAnalyser {
      * @return
      */
     public Map<String, String[]> lookahead(String[] word) {
+
+
+        // if word = determiner, return <word, adjectives[] || noun[]>
+        // if word = an adjective, return <word, nouns[]>
+        // if word = a noun, return <word, verb[]>
         return null;
     }
 }
