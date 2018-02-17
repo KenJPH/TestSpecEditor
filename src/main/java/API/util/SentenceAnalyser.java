dbcac195b0208a842d6494cb3ad7511e84504cf4
Sam <sam.grennan8@gmail.com>
2018-02-17 11:46:46 +1100

    validation for basic sentence structure syntax S-V-O implemented

--- src/main/java/API/util/SentenceAnalyser.java
+++ src/main/java/API/util/SentenceAnalyser.java
@@ -1,91 +1,74 @@
 package API.util;
 
-import java.util.Arrays;
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class SentenceAnalyser {
 
-    private String[] nouns = new String[20];
-    private String[] determiners = new String[20];
-    private String[] adjectives = new String[20];
+    String objectNoun;
+    String subjectNoun;
+    String verb;
+    String preposition;
+    String determiner;
+
+    String[] array = {"Given", "user", "navigates", "website"};
+    String[] array1 = {"When", "user", "searches", "name"};
+    String[] array2 = {"And", "user", "presses", "search"};
+    String[] array3 = {"Then", "user", "see", "results"};
 
 
     /**
      * Validates the correctness of a sentences syntax
      *
+     * This validates the correct of sentence syntax for basic sentences that take on the form S: {subject, verb, object}
+     *
      * @param sentence
      * @return
      */
     public boolean validate(String[] sentence) {
-
+        for (int i = 0; i < sentence.length; i++) {
+            if (sentence[0] == "Given" || sentence[0] == "When" || sentence[0] == "And" || sentence[0] == "Then") {
+                if (sentence[1] != objectNoun && sentence[2] != verb && sentence[3] != subjectNoun) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
 
     /**
      * Given a word, provide a list of words that can be followed by it (lookahead words)
-     *
+     * <p>
      * For example: word = The
-     *              return = {{adjective: fast, slow, big, quick}, {noun: computer, laptop, server}, etc..}
+     * return = {{adjective: fast, slow, big, quick}, {noun: computer, laptop, server}, etc..}
      *
      * @param word
      * @return
      */
     public Map<String, String[]> lookahead(String word) {
-        Map<String, String[]> detLookahead = new HashMap<>();
-
-        if (Arrays.asList(determiners).contains(word)) {
-            detLookahead.put(word, adjsnouns(nouns, adjectives));
-            return detLookahead;
-        }
-
-    }
+        Map<String, String[]> tmp = new HashMap<>();
 
 
-    /**
-     * Helper method that concatenates array of nouns and adjectives
-     *
-     * Nouns and adjectives are the only word type that can follow a determiner such as "the" or "a"
-     *
-     * @param adj array of adjs
-     * @param nouns array of nouns
-     * @return arrays concatenated as one array
-     */
-    public String[] adjsnouns(String[] adj, String[] nouns) {
-        String[] result = new String[nouns.length + adj.length];
-        for (int i = 0; i < nouns.length; i++) {
-            result[i] = nouns[i];
-        }
-        for (int j = 0; j < adj.length; j++) {
-            result[j + 20] = adj[j];
-        }
-        return result;
-    }
-
-
-    public Map<String, String[]> detLookahead(String word) {
         return null;
     }
 
 
     /**
      * Same as above but input is a full sentence.
-     *
+     * <p>
      * Note, providing only the last word will not be sufficient
      * Investigate the grouping of lexical categories and see if you can derive a proof system
-     *
+     * <p>
      * This is bonus work that we can look at later
      *
      * @param word
      * @return
      */
     public Map<String, String[]> lookahead(String[] word) {
-
-
-        // if word = determiner, return <word, adjectives[] || noun[]>
-        // if word = an adjective, return <word, nouns[]>
-        // if word = a noun, return <word, verb[]>
         return null;
     }
 }
